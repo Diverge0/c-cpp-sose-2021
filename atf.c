18d68fa873a4182332d0ee61a4b867efb190f95f
Henning Ohlmeyer <henning@die-ohlmeyers.de>
2021-08-04 16:24:06 +0200

    quicksave: in the middle of doing generate_search_spaces(...)

--- atf.c
+++ atf.c
@@ -10,12 +10,55 @@ void print_configuration(int* values, int num_values) {
 }
 
 tp_t create_tp(char* name, int min, int max, constraint_t constraint) {
-  // Ergänzen Sie hier Ihre Lösung für Aufgabe 1
+  tp_t newtpt = {.name = name, .min = min, .max = max, .constraint = constraint};
+  return newtpt;
+}
+
+//todo: add to header file
+void count_up(int num_parameters, int* currentIndex, int* maxIndex, int current){
+  if()
 }
 
 void generate_search_space(tp_t* parameters, int num_parameters,
                            search_space_t* search_space) {
-  // Ergänzen Sie hier Ihre Lösung für Aufgabe 1
+  int currentIndex[num_parameters];
+  int maxIndex[num_parameters];
+  for(int i=0; i<num_parameters;i++){
+    currentIndex[i] = parameters[i].min;
+    maxIndex[i] = parameters[i].max;
+  }
+
+  //check if configuration is valid
+  while(~~~){
+    int isValid = 1;
+    int const_param[10];
+
+    //das geht bestimmt irgendwie schöner
+    for(int i=0; i<num_parameters; i++){
+      const_param[i] = currentIndex[i];
+    }
+
+    //test constraints
+    for(int i=0; i<num_parameters; i++){
+      if(parameters[i].constraint != NULL){
+        if(!parameters[i].constraint(const_param[0], const_param[1], const_param[2], 
+                                    const_param[3], const_param[4], const_param[5], 
+                                    const_param[6], const_param[7], const_param[8], 
+                                    const_param[9])){
+          isValid = 0;
+        }
+        
+      }
+    }
+    if(isValid){
+      print_configuration(currentIndex, num_parameters);
+    }
+
+    //count up
+    for(int i=num_parameters; i>=0; i--){
+      if()
+    }
+  }
 }
 configuration_t get_config(search_space_t* search_space, int index) {
   // Ergänzen Sie hier Ihre Lösung für Aufgabe 2
